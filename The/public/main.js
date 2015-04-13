9061aca331328798addea49c9d7213676aded460
Dan Stepanov <danhstepanov@gmail.com>
2015-04-13 17:41:33 -0400

    testing renamed function

--- The/public/main.js
+++ The/public/main.js
@@ -1,19 +1,27 @@
-function submitFunction() {
-	console.log("submitFunction executed");
-	Parse.initialize("9oSP3PB2jbHOtMg3p0PilW9tQ6ahbfUvlEc9DmYQ", "IhLrkVUv2hGqYadeTW2rk2tOPCPhIgHFDlJO5tnq");
-	
+funciton getHacks() {
+    console.log("getHacks executed");
+    console.log("getHacks completed");
+};
+
+function submitHack() {
+	console.log("submitHack executed");
+	Parse.initialize("9oSP3PB2jbHOtMg3p0PilW9tQ6ahbfUvlEc9DmYQ", "IhLrkVUv2hGqYadeTW2rk2tOPCPhIgHFDlJO5tnq");	
 	var Posts = Parse.Object.extend("Posts");
 	var post = new Posts();
 	var title = $("input[name='title']").val();
+    var description = $("input[name='description']").val();
 	var link = $("input[name='link']").val();
 	post.save({
 		title: title,
+        description: description,
 		link: link,
 		upvote: 0,
 	}).then(function(object) {
 		console.log("great success!");
 		$("input[name='title']").val('');
+        $("input[name='description']").val('');
 		$("input[name='link']").val('');
 	});
-	console.log("submitFunction completed");
+	console.log("submitHack completed");
+    
 };
